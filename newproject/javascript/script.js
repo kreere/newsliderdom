0f7445dce4554dd1820336215921d81acd44ed66
kreere <zlkathesla@gmail.com>
2018-03-28 17:51:14 +0300

    qw

--- newproject/javascript/script.js
+++ newproject/javascript/script.js
@@ -1,30 +1,12 @@
-var goods = {
-	"3344" : {
-		"name": "Banana",
-		"cost": 30,
-		"img": "https://cdn2.iconfinder.com/data/icons/jetflat-fruits/90/002_014_banana_banan_plant_herb_fruit_food-128.png",
-		"warehouse" : "yes"
-	},
-	"3345" : {
-		"name": "Tomato",
-		"cost": 40,
-		"img": "https://cdn0.iconfinder.com/data/icons/fruits/128/Tomato.png",
-		"warehouse" : "yes"
-	},
-	"334532" : {
-		"name": "Apple",
-		"cost": 45,
-		"img": "https://cdn3.iconfinder.com/data/icons/fruits-8/512/apple-128.png",
-		"warehouse" : "yes"
-	}
-};
-console.log(goods);
-var out = '';
-for(var key in goods) {
-	out+='Name: '+goods[key].name +'<br>';
-	out+='Price: '+goods[key].cost +'<br>';
-	out+='In stock: '+goods[key].warehouse +'<br>';
-	out+='<img src="'+goods[key].img+'">';
-	out+='<hr>';	
+document.onmousemove = function() {
+	document.getElementsByTagName('body')[0].insertAdjacentHTML('beforeEnd', '<img src="https://cdn4.iconfinder.com/data/icons/small-n-flat/24/cat-48.png" id="cat">');
+	var cat = document.getElementById('cat');
+	document.onmousemove = function(event) {
+	cat.style.position = 'fixed';
+	cat.style.left = event.clientX + 20+'px'
+	cat.style.top = event.clientY + 20+'px'
 }
-document.getElementById('out').innerHTML = out;
+}
+
+
+
