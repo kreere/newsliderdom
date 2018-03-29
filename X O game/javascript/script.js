f78fd3bbe902240b39a58d2e6cb53bd6041e6b8e
kreere <zlkathesla@gmail.com>
2018-03-29 17:24:04 +0300

    adding reload

--- X O game/javascript/script.js
+++ X O game/javascript/script.js
@@ -1,18 +1,18 @@
 for (var i = 0; i <9; i++) {
     document.getElementById('game').innerHTML+='<div class="block"></div>';
 }
-var hod = 0;
+var turn = 0;
 
 document.getElementById('game').onclick = function(event) {
     console.log(event);
     if (event.target.className == 'block') {
-        if (hod % 2 == 0) {
+        if (turn % 2 == 0) {
             event.target.innerHTML = 'x';
         }
         else {
             event.target.innerHTML = 'o';
         }
-        hod++;
+        turn++;
         checkWinner();
     }
 }
@@ -20,45 +20,59 @@ function checkWinner() {
     var allblock = document.getElementsByClassName('block');
     console.log(allblock);
     if (allblock[0].innerHTML=='x' && allblock[1].innerHTML=='x' && allblock[2].innerHTML=='x') {
-     alert('X won')
+     alert('X won, this page will be reloaded')
+     location.reload();
     }
     if (allblock[3].innerHTML=='x' && allblock[4].innerHTML=='x' && allblock[5].innerHTML=='x') {
-     alert('X won')
+     alert('X won, this page will be reloaded')
+     location.reload();
     }
     if (allblock[6].innerHTML=='x' && allblock[7].innerHTML=='x' && allblock[8].innerHTML=='x') {
-     alert('X won')
+     alert('X won, this page will be reloaded')
+     location.reload();
     }
     if (allblock[0].innerHTML=='x' && allblock[3].innerHTML=='x' && allblock[6].innerHTML=='x') {
-     alert('X won')
+     alert('X won, this page will be reloaded')
+     location.reload();
     }
     if (allblock[1].innerHTML=='x' && allblock[4].innerHTML=='x' && allblock[7].innerHTML=='x') {
-     alert('X won')
+     alert('X won, this page will be reloaded')
+     location.reload();
     }
     if (allblock[2].innerHTML=='x' && allblock[5].innerHTML=='x' && allblock[8].innerHTML=='x') {
-     alert('X won')
+     alert('X won, this page will be reloaded')
+     location.reload();
     }
     if (allblock[0].innerHTML=='x' && allblock[4].innerHTML=='x' && allblock[8].innerHTML=='x') {
-     alert('X won')
+     alert('X won, this page will be reloaded')
+     location.reload();
     }
     if (allblock[0].innerHTML=='o' && allblock[1].innerHTML=='o' && allblock[2].innerHTML=='o') {
-     alert('O won')
+     alert('O won, this page will be reloaded')
+     location.reload();
     }
     if (allblock[3].innerHTML=='o' && allblock[4].innerHTML=='o' && allblock[5].innerHTML=='o') {
-     alert('O won')
+     alert('O won, this page will be reloaded')
+     location.reload();
     }
     if (allblock[6].innerHTML=='o' && allblock[7].innerHTML=='o' && allblock[8].innerHTML=='o') {
-     alert('O won')
+     alert('O won, this page will be reloaded')
+     location.reload();
     }
     if (allblock[0].innerHTML=='o' && allblock[3].innerHTML=='o' && allblock[6].innerHTML=='o') {
-     alert('O won')
+     alert('O won, this page will be reloaded')
+     location.reload();
     }
     if (allblock[1].innerHTML=='o' && allblock[4].innerHTML=='o' && allblock[7].innerHTML=='o') {
-     alert('O won')
+     alert('O won, this page will be reloaded')
+     location.reload();
     }
     if (allblock[2].innerHTML=='o' && allblock[5].innerHTML=='o' && allblock[8].innerHTML=='o') {
-     alert('O won')
+     alert('O won, this page will be reloaded')
+    location.reload();
     }
     if (allblock[0].innerHTML=='o' && allblock[4].innerHTML=='o' && allblock[8].innerHTML=='o') {
-     alert('O won')
+     alert('O won, this page will be reloaded')
+    location.reload();
     }
 }
